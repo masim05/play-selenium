91e8c7106779ceb34017e99af7af635b9f61c382
Max Ryndin <rgmasim@gmail.com>
2023-08-19 22:30:38 +0700

    Add network tracing.

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
+const http = require('http')
 const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.static('public'))
 app.get('/', (req, res) => {
+  res.send('Hello World!!!')
+})
+// submit
+app.get('/submit', (req, res) => {
+    console.log(req.rawBody)
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+const web = http.createServer(app)
+
+web.on("connection", (socket) => {
+    console.log("==connection")
+
+    data = ''
+    socket.on('data', (chunk) => {
+        data += chunk.toString('utf8');
+    })
+    socket.on("close", () => {
+        console.log(data)
+        console.log("==close")
+    });
+});
+
+web.listen(3000, () => {
+    console.log("Running")
+});
